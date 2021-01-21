0658a5b9f3391e5c13313a66c0fdc00c723e836c
Yuna Kusaka <loxoo_1@yahoo.co.jp>
2021-01-21 22:01:24 +0900

    create director CRUD

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,52 @@
+class DirectorsController < ApplicationController
+  before_action :set_director, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @directors = Director.all
+  end
+
+  def new
+    @director = Director.new
+  end
+
+  def create
+    @director = Director.new(director_params)
+    if @director.save
+      redirect_to directors_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @director.update(director_params)
+      redirect_to director_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @director.destroy
+    redirect_to directors_path
+  end
+
+  private
+
+  def set_director
+    @director = Director.find(params[:id])
+  end
+
+  def director_params
+    params.require(:director).permit(
+      :name,
+      :country
+    )
+  end
+end
