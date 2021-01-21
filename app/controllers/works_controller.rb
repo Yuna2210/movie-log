c7d094031e236b7b8903db62a612abf2f10f6c68
Yuna Kusaka <loxoo_1@yahoo.co.jp>
2021-01-21 22:18:48 +0900

    create works CRUD

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,54 @@
+class WorksController < ApplicationController
+  before_action :set_work, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @works = Work.all
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @work.update(work_params)
+      redirect_to work_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to works_path
+  end
+
+  private
+
+  def set_work
+    @work = Work.find(params[:id])
+  end
+
+  def work_params
+    params.require(:work).permit(
+      :title,
+      :relese,
+      :main_actor,
+      :description,
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'home/index' => 'home#index'
 
   resources :directors
+  resources :works
 end
