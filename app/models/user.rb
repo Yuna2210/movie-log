6708cfec07243f013f8fb0e1343b559ba5177b77
ayako_tachibana <ayako.tachibana0328@gmail.com>
2021-02-09 15:03:12 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def display_name
-    if profile.nil?
+    unless profile.present?
       "Unknown"
     else
       profile.nickname.present? ? profile.nickname : profile.first_name
